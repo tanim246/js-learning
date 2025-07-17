cb967411f8faa0c06b16eb9f187bd066004df0fa
Tanim <mdtasnimhossaintanim@gmail.com>
2025-07-17 17:50:12 +0600

    add find.js file

---
+++ find.js
@@ -0,0 +1,34 @@
+const arr = ["tanim", "sadman", "alok", "deep"];
+
+// find
+
+// let value = arr.find((person=>console.log(person)))
+
+// arr.find((person)=>console.log(person))
+
+// arr.map((person)=>console.log(person))
+
+const numbers = [10, 12, 50, 22, 32, 40];
+
+// const num = numbers.find((n)=>n>22)
+// console.log(num)
+
+// filter
+
+// const numFilter = numbers.filter((n)=>n>22)
+// console.log(numFilter)
+
+// map
+
+const arrOfObj = [
+  { name: "tahim", age: 20 },
+  { name: "sadman", age: 25 },
+  { name: "uzzol", age: 25 },
+  { name: "deep", age: 25 },
+  { name: "jami", age: 25 },
+  { name: "mahin", age: 25 },
+];
+// console.log(obj)
+
+const data = arrOfObj.map((person)=>console.log(person.name));
+
